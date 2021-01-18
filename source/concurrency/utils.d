d453f922f18aea737fcd90f3849870eef03d1f3f
Sebastiaan Koppe <mail@skoppe.eu>
2021-01-18 13:09:38 +0100

    Accept nothrow and @trusted function in manual closure
    Also propagate the nothrow attribute if present

--- source/concurrency/utils.d
+++ source/concurrency/utils.d
@@ -1,19 +1,26 @@
 module concurrency.utils;
 
+import std.traits;
+
 /// A manually constructed closure, aimed at shared
 struct Closure(Fun, Args...) {
   Fun fun;
   Args args;
-  auto apply() shared @trusted {
+  auto apply() shared {
     return fun((cast()this).args);
   }
 }
 
-auto closure(Args...)(void function(Args) @safe fun, Args args) @trusted {
-  alias Fun = typeof(fun);
-  auto cl = new Closure!(Fun, Args)(fun, args);
+/// This is a terrible workaround for closure bugs in D. When a local is used in a delegate D is supposed to move it to the heap. I haven't seen that happen in all cases so we have this manual workaround.
+auto closure(Fun, Args...)(Fun fun, Args args) @trusted if (isFunctionPointer!Fun) {
+  static assert(!hasFunctionAttributes!(Fun, "@system"), "no @system functions please");
+  auto cl = cast(shared)new Closure!(Fun, Args)(fun, args);
   /// need to cast to @safe because a @trusted delegate doesn't fit a @safe one...
-  return cast(void delegate() shared @safe)&(cast(shared)cl).apply;
+  static if (hasFunctionAttributes!(Fun, "nothrow"))
+    alias ResultType = void delegate() nothrow shared @safe;
+  else
+    alias ResultType = void delegate() shared @safe;
+  return cast(ResultType)&cl.apply;
 }
 
 /// don't want vibe-d to overwrite the scheduler
