cf589b3553634e181da42dd953c9c9e36cbae1ee
Sebastiaan Koppe <mail@skoppe.eu>
2021-01-20 11:46:03 +0100

    Revert "Remove unittest version out of LocalThreadExecutor"
    This reverts commit 7a6ba799217346719c93f83bec14ccc308c66923.
    
    Apparently it is needed to run the unittests properly.
    
    Luckily it won't impact downstream users as the unittest version block
    are only applied when the concurrency package itself is compiling in
    unittest mode.

--- source/concurrency/thread.d
+++ source/concurrency/thread.d
@@ -25,17 +25,28 @@ class LocalThreadExecutor : Executor {
   }
 
   void execute(VoidDelegate dg) @trusted {
-    if (isInContext)
+    version (unittest) {
+      // NOTE: We must call the delegate on the current thread instead of going to the main one
+      // the reason is that there is no nursery running on the main one because we are running unit tests
+      // when SIL becomes multithreaded we can revisit this
       dg();
-    else
-      (cast() tid).send(dg);
+    } else {
+      if (isInContext)
+        dg();
+      else
+        (cast() tid).send(dg);
+    }
   }
 
   void execute(VoidFunction fn) @trusted {
-    if (isInContext)
+    version (unittest) {
       fn();
-    else
-      (cast() tid).send(fn);
+    } else {
+      if (isInContext)
+        fn();
+      else
+        (cast() tid).send(fn);
+    }
   }
 
   void start() @trusted {
