ab00c96e05fa2becb4ad5bf97b026bf8dafa1e1b
Sebastiaan Koppe <mail@skoppe.eu>
2022-08-22 12:30:26 +0200

    Rewrite throttleLast to use ManualTimeWorker

--- tests/ut/concurrency/stream.d
+++ tests/ut/concurrency/stream.d
@@ -327,18 +327,36 @@ import concurrency.thread : ThreadSender;
 @("throttling.throttleLast")
 @safe unittest {
   import core.time;
+  import concurrency.scheduler : ManualTimeWorker;
+  import concurrency.operations : withScheduler, whenAll;
+  import concurrency.sender : justFrom;
 
   shared int p = 0;
+  auto worker = new shared ManualTimeWorker();
 
-  1.msecs
-    .intervalStream()
+  auto throttled = 1.msecs
+    .intervalStream(true)
     .scan((int acc) => acc+1, 0)
     .throttleLast(3.msecs)
-    .take(6)
+    .take(4)
     .collect((int i) shared { p.atomicOp!"+="(i); })
-    .syncWait().assumeOk;
+    .withScheduler(worker.getScheduler);
 
-  p.atomicLoad.shouldBeGreaterThan(40);
+  auto driver = justFrom(() shared {
+      p.atomicLoad.should == 0;
+      worker.timeUntilNextEvent().should == 1.msecs;
+
+      foreach(expected; [0,0,3,3,3,9,9,9,18,18,18,30]) {
+        worker.advance(1.msecs);
+        p.atomicLoad.should == expected;
+      }
+
+      worker.timeUntilNextEvent().should == null;
+  });
+
+  whenAll(throttled, driver).syncWait().assumeOk;
+
+  p.atomicLoad.should == 30;
 }
 
 @("throttling.throttleLast.arrayStream")
@@ -369,19 +387,37 @@ import concurrency.thread : ThreadSender;
 @("throttling.throttleLast.thread")
 @safe unittest {
   import core.time;
+  import concurrency.scheduler : ManualTimeWorker;
+  import concurrency.operations : withScheduler, whenAll;
+  import concurrency.sender : justFrom;
 
   shared int p = 0;
+  auto worker = new shared ManualTimeWorker();
 
-  1.msecs
-    .intervalStream()
+  auto throttled = 1.msecs
+    .intervalStream(true)
     .via(ThreadSender())
     .scan((int acc) => acc+1, 0)
     .throttleLast(3.msecs)
-    .take(6)
+    .take(4)
     .collect((int i) shared { p.atomicOp!"+="(i); })
-    .syncWait().assumeOk;
+    .withScheduler(worker.getScheduler);
 
-  p.atomicLoad.shouldBeGreaterThan(40);
+  auto driver = justFrom(() shared @trusted {
+      worker.wait();
+      p.atomicLoad.should == 0;
+
+      foreach(expected; [0,0,3,3,3,9,9,9,18,18,18,30]) {
+        worker.advance(1.msecs);
+        p.atomicLoad.should == expected;
+      }
+
+      worker.timeUntilNextEvent().should == null;
+    });
+
+  whenAll(throttled, driver).syncWait().assumeOk;
+
+  p.atomicLoad.should == 30;
 }
 
 @("throttling.throttleLast.thread.arrayStream")
